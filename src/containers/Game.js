2de24beda7f68593eb13149a842129cd41438ac8
loezzje <m.demmendaal@hotmail.com>
2017-10-27 10:19:10 +0200

    create game container

---
+++ src/containers/Game.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+
+
+class GameContainer extends PureComponent {
+  render() {
+    return(
+      <h1> Game </h1>
+    )
+  }
+}
+
+export default GameContainer
